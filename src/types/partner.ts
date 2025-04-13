d3ef504b95c717d0eeae3798f9965bd07ee21ab8
thisisbriandb <evelinbrid@gmail.com>
2025-04-13 22:24:34 +0200

    fix next.js 15 error

--- src/types/partner.ts
+++ src/types/partner.ts
@@ -1,11 +1,20 @@
 export interface Partner {
   id: string
   name: string
+  shortDesc: string
   description: string
+  logo: string
   image: string
+  images: string[]
   location: string
   specialties?: string[]
+  chirurgicalSpecialties?: string[]
   phone?: string
   website?: string
   email?: string
+  quote?: {
+    text: string
+    author: string
+  }
+  highlights?: string[]
 } 
